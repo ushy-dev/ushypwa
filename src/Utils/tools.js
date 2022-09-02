4b093341754866f27aa88857c89af29368756b40
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-09-02 04:01:40 +0300

    Update tools.js

--- src/Utils/tools.js
+++ src/Utils/tools.js
@@ -55,8 +55,8 @@ export const moreIcon = (
 
 export const arrowdownbutton = (
   <svg
-    width="24"
-    height="24"
+    width="18"
+    height="18"
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
@@ -72,8 +72,8 @@ export const arrowdownbutton = (
 
 export const gigIconActive = (
   <svg
-    width="18"
-    height="18"
+    width="26"
+    height="26"
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
