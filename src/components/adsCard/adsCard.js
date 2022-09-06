15a15560676f4601bb48abb336cddc8849ccddf6
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-09-06 14:26:35 +0300

    added ads

--- src/components/adsCard/adsCard.js
+++ src/components/adsCard/adsCard.js
@@ -1,18 +1,17 @@
 import React from "react";
+import ads1 from "../../assets/images/ads1.png";
+import ads2 from "../../assets/images/ads2.png";
+import ads3 from "../../assets/images/ads3.png";
 import "./adsCard.css";
 
 const AdsCard = () => {
   return (
     <div className="adscard__container">
-      <div className="adsCard-item ads_a">
-        <p>Ad Space</p>
-      </div>
-      <div className="adsCard-item ads_b">
-        <p>Ad Space</p>
-      </div>
-      <div className="adsCard-item ads_c">
-        <p>Ad Space</p>
-      </div>
+      <img className="adsCard-item ads_a" src={ads1} alt="" />
+
+      <img className="adsCard-item ads_b" src={ads2} alt="" />
+
+      <img className="adsCard-item ads_c" src={ads3} alt="" />
     </div>
   );
 };
